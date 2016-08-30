f8a0458b149ad4517a67c56533658dd003c2db6b
frozenfung <frozenfung@gmail.com>
2016-08-30 16:41:45 +0800

    Ignore bower and related files

--- sample/bower_components/jquery-tinyslide/spec/index.js
+++
@@ -1,9 +0,0 @@
-const TinySlide = require('../index.js');
-
-describe('jquery-tinySlide', () => {
-  "use strict";
-
-  it('should be defined', () => {
-    expect(TinySlide).toBeDefined();
-  });
-});
